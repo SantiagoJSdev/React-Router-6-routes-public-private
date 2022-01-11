08050352c74a926598bf2143ada5c193cb620dbc
SantiagoJSdev <santiagoreyesmarin1986@gmail.com>
2022-01-11 16:50:00 -0500

    ready

---
+++ docs/precache-manifest.d3c8730f4237363c0e9cece4a7204027.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38bfd5903297e5785f9d44a5cf85f609",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c3946a0f12b7f0f0e44f",
+    "url": "/static/css/main.16f4cb8a.chunk.css"
+  },
+  {
+    "revision": "9878fc2dd1c9d0939b36",
+    "url": "/static/js/2.5db3467e.chunk.js"
+  },
+  {
+    "revision": "83dd22b0688da01307759f0db1576406",
+    "url": "/static/js/2.5db3467e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3946a0f12b7f0f0e44f",
+    "url": "/static/js/main.5d2a0f80.chunk.js"
+  },
+  {
+    "revision": "ede06adfc8d41a12f0e7",
+    "url": "/static/js/runtime-main.c607270d.js"
+  }
+]);
